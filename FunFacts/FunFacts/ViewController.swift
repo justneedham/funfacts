e68a67ddf8f0fc8a5c3513d7cbfad2f079b660b6
Justin Needham <justin.needham@alumnimail.pepperdine.edu>
2019-02-02 12:56:07 -0700

    project setup

---
+++ FunFacts/FunFacts/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Justin Needham on 2019-02-01.
+//  Copyright © 2019 ___Treehouse___. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    @IBOutlet weak var funFactLabel: UILabel!
+    
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
