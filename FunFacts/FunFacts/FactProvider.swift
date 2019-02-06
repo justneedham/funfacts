465e4161a8d83d61e068704aa8d71c366d405c53
Justin Needham <justin.needham@alumnimail.pepperdine.edu>
2019-02-06 13:07:28 -0700

    typo

--- FunFacts/FunFacts/FactProvider.swift
+++ FunFacts/FunFacts/FactProvider.swift
@@ -14,7 +14,7 @@ struct FactProvider {
         "On average, it takes 66 days to form a new habit",
         "Some penguins can leap 2-3 meters out of the water",
         "The state of Florida is bigger than England",
-        "Some bamboo plantes can grow almost a meter in just one day",
+        "Some bamboo plants can grow almost a meter in just one day",
         "It takes about 8 minutes for the light from Sun to reach Earth",
         "You are born with 300 bones; by the time you are an adult you will 206"
     ]
